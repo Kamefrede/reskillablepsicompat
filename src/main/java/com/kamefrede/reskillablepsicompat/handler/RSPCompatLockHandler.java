af4d052e54100f61ced50e766c6b1387a18d6b4c
Kamefrede <kamefrede@gmail.com>
2019-01-11 16:14:12 +0000

    1.2.222222222222222222222

--- src/main/java/com/kamefrede/reskillablepsicompat/handler/RSPCompatLockHandler.java
+++ src/main/java/com/kamefrede/reskillablepsicompat/handler/RSPCompatLockHandler.java
@@ -1,46 +1,52 @@
 package com.kamefrede.reskillablepsicompat.handler;
 
 import codersafterdark.reskillable.base.ConfigHandler;
+import codersafterdark.reskillable.base.LevelLockHandler;
 import codersafterdark.reskillable.network.MessageLockedItem;
 import com.kamefrede.reskillablepsicompat.RSPCompat;
 import com.kamefrede.reskillablepsicompat.util.RSPConfigHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import vazkii.psi.api.cad.CADTakeEvent;
 import vazkii.psi.api.cad.ICAD;
 import vazkii.psi.api.spell.PreSpellCastEvent;
 
+import java.util.function.Consumer;
+
 import static codersafterdark.reskillable.base.LevelLockHandler.*;
 
 @Mod.EventBusSubscriber(modid = RSPCompat.MODID)
 public class RSPCompatLockHandler {
 
-    private static final String TAG_CHECKED = "militaryGradeCertified";
-
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void preSpellCastEvent(PreSpellCastEvent event) {
-        genericEnforce(event, event.getPlayer(), event.getCad(), MessageLockedItem.MSG_ITEM_LOCKED);
+        if (!event.getPlayer().world.isRemote)
+            genericEnforce(event::setCanceled, event::setCancellationMessage, event.getPlayer(), event.getCad(), MessageLockedItem.MSG_ITEM_LOCKED, "rspcompat.error.castspell");
+
+
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public static void takeCadEvent(CADTakeEvent event) {
-        if (!event.getCad().isEmpty() && event.getCad().getItem() instanceof ICAD)
-            takeEnforce(event, event.getPlayer(), event.getCad(), MessageLockedItem.MSG_ITEM_LOCKED);
+        if (!event.getCad().isEmpty() && event.getCad().getItem() instanceof ICAD && RSPConfigHandler.disableTaking)
+            genericEnforce(event::setCanceled, event::setCancellationMessage, event.getPlayer(), event.getCad(), MessageLockedItem.MSG_ITEM_LOCKED, "rspcompat.error.takecad");
     }
 
     //copypasta from LevelLockHandler just to add a line
-    public static void genericEnforce(Event event, EntityPlayer player, ItemStack stack, String lockMessage) {
-        if (!event.isCancelable() || event.isCanceled() || player == null || stack == null || stack.isEmpty() || (!ConfigHandler.enforceOnCreative && player.isCreative())) {
+
+    public static void genericEnforce(Consumer<Boolean> cancel, Consumer<String> cancellationMessage, EntityPlayer player, ItemStack stack, String lockMessage, String cancelMessage) {
+        if (player == null || (!ConfigHandler.enforceOnCreative && player.capabilities.isCreativeMode))
             return;
-        }
+
         if (ConfigHandler.enforceFakePlayers) {
             if (!canPlayerUseItem(player, stack)) {
-                event.setCanceled(true);
+                cancel.accept(true);
+                cancellationMessage.accept(cancelMessage);
                 if (!isFake(player)) {
                     player.getCooldownTracker().setCooldown(stack.getItem(), 40);
                     tellPlayer(player, stack, lockMessage);
@@ -49,14 +55,48 @@ public class RSPCompatLockHandler {
         } else if (!isFake(player) && !canPlayerUseItem(player, stack)) {
             player.getCooldownTracker().setCooldown(stack.getItem(), 40);
             tellPlayer(player, stack, lockMessage);
+            cancel.accept(true);
+            cancellationMessage.accept(cancelMessage);
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void rightClickItemOverride(PlayerInteractEvent.RightClickItem event) {
+        if (!event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof ICAD && event.getEntityPlayer() != null && !LevelLockHandler.canPlayerUseItem(event.getEntityPlayer(), event.getItemStack()))
             event.setCanceled(true);
-        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
+    public static void rightClickItemOverrideBoogaloo(PlayerInteractEvent.RightClickItem event) {
+        if (event.isCanceled() && !event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof ICAD && event.getEntityPlayer() != null && !LevelLockHandler.canPlayerUseItem(event.getEntityPlayer(), event.getItemStack()))
+            event.setCanceled(false);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void rightClickBlockOverride(PlayerInteractEvent.RightClickBlock event) {
+        if (!event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof ICAD && event.getEntityPlayer() != null && !LevelLockHandler.canPlayerUseItem(event.getEntityPlayer(), event.getItemStack()))
+            event.setCanceled(true);
+    }
+
+    @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
+    public static void rightClickBlockOverrideBoogaloo(PlayerInteractEvent.RightClickBlock event) {
+        if (event.isCanceled() && !event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof ICAD && event.getEntityPlayer() != null && !LevelLockHandler.canPlayerUseItem(event.getEntityPlayer(), event.getItemStack()))
+            event.setCanceled(false);
+    }
+
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public static void entityInteractOverride(PlayerInteractEvent.EntityInteract event) {
+        if (!event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof ICAD && event.getEntityPlayer() != null && !LevelLockHandler.canPlayerUseItem(event.getEntityPlayer(), event.getItemStack()))
+            event.setCanceled(true);
+    }
+
+    @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
+    public static void entityInteractOverrideBoogaloo(PlayerInteractEvent.EntityInteract event) {
+        if (event.isCanceled() && !event.getItemStack().isEmpty() && event.getItemStack().getItem() instanceof ICAD && event.getEntityPlayer() != null && !LevelLockHandler.canPlayerUseItem(event.getEntityPlayer(), event.getItemStack()))
+            event.setCanceled(false);
     }
 
 
-    public static void takeEnforce(Event event, EntityPlayer player, ItemStack stack, String lockMessage) {
-        if (RSPConfigHandler.disableTaking) {
-            genericEnforce(event, player, stack, lockMessage);
-        }
-    }
+
+
 }
